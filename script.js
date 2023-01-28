b97996e28989b299debe64a809914484acc334f3
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-01-28 14:52:14 -0600

    d

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 33.208, lng: -97.156 },
-      zoom: 14.7,
+      zoom: 4,
       mapid: '576037077fe48406'
     });
   }
