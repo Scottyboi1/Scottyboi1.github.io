f0359c7add4b71453736d9b64ac8ef736d0966e7
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-01-28 14:39:30 -0600

    all

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-// Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -25.344, lng: 131.031 },
       zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
+      mapid: '576037077fe48406'
     });
   }
-  
-  window.initMap = initMap;
