7aa27459755f7398207afd139914551c4bd03511
Scottyboi1 <96198769+Scottyboi1@users.noreply.github.com>
2023-02-27 10:37:32 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 33.20750461273979, lng: -97.15295817275108 },
-      zoom: 14.7,
-      mapid: '576037077fe48406'
-    });
-  }
-
-  //33.20750461273979, -97.15295817275108
